6a51c9569afd7b8c50afaaaef646f7e4395cf463
leswlz <54888222+leswlz@users.noreply.github.com>
2021-07-04 23:23:54 +0300

    lateral movement

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from kivy.config import Config
+Config.set('graphics', 'width', '900')
+Config.set('graphics', 'height', '400')
+
 from kivy.app import App
 from kivy.graphics.context_instructions import Color
 from kivy.graphics.vertex_instructions import Line
@@ -19,6 +23,9 @@ class MainWidget(Widget):
 
     SPEED = 4
     current_offset_y = 0
+
+    SPEED_X = 3
+    current_offset_x = 0
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
@@ -59,7 +66,7 @@ class MainWidget(Widget):
         spacing = self.V_LINES_SPACING * self.width
         offset = -int(self.V_NB_LINES/2) + 0.5
         for i in range(0, self.V_NB_LINES):
-            line_x = int(center_line_x + offset * spacing)
+            line_x = center_line_x + offset * spacing + self.current_offset_x
 
             x1, y1 = self.transform(line_x, 0)
             x2, y2 = self.transform(line_x, self.height)
@@ -77,8 +84,8 @@ class MainWidget(Widget):
         spacing = self.V_LINES_SPACING * self.width
         offset = int(self.V_NB_LINES/2) - 0.5
 
-        xmin = center_line_x - offset * spacing
-        xmax = center_line_x + offset * spacing
+        xmin = center_line_x - offset * spacing + self.current_offset_x
+        xmax = center_line_x + offset * spacing + self.current_offset_x
         spacing_y = self.H_LINES_SPACING * self.height
 
         for i in range(0, self.H_NB_LINES):
@@ -119,6 +126,8 @@ class MainWidget(Widget):
         spacing_y = self.H_LINES_SPACING * self.height
         if self.current_offset_y >= spacing_y:
             self.current_offset_y -= spacing_y
+
+        self.current_offset_x += self.SPEED_X * time_factor
 
 class GalaxyApp(App):
     pass
