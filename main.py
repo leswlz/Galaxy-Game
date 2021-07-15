380bc840a1ae9f5ce147cf61719c04be88230ead
leswlz <54888222+leswlz@users.noreply.github.com>
2021-07-15 12:58:15 +0300

    fixing the speed

--- main.py
+++ main.py
@@ -21,18 +21,18 @@ class MainWidget(Widget):
     perspective_point_y = NumericProperty(0)
 
     V_NB_LINES = 8  # number of vertical lines
-    V_LINES_SPACING = .2  # percentage of the screen width
+    V_LINES_SPACING = .4  # percentage of the screen width
     vertical_lines = []
 
     H_NB_LINES = 15  # number of horizontal lines
     H_LINES_SPACING = .1  # percentage of the screen height
     horizontal_lines = []
 
-    SPEED = 2
+    SPEED = .8
     current_offset_y = 0
     current_y_loop = 0
 
-    SPEED_X = 12
+    SPEED_X = 3.0
     current_speed_x = 0
     current_offset_x = 0
 
@@ -200,13 +200,14 @@ class MainWidget(Widget):
             self.horizontal_lines[i].points = [x1, y1, x2, y2]
 
     def update(self, dt):
-        # print("dt: " + str(dt * 60))
         time_factor = dt * 60
         self.update_vertical_lines()
         self.update_horizontal_lines()
         self.update_tiles()
         self.update_ship()
-        self.current_offset_y += self.SPEED * time_factor
+
+        speed_y = self.SPEED * self.height / 100
+        self.current_offset_y += speed_y * time_factor
 
         spacing_y = self.H_LINES_SPACING * self.height
         if self.current_offset_y >= spacing_y:
@@ -214,7 +215,8 @@ class MainWidget(Widget):
             self.current_y_loop += 1
             self.generate_tiles_coordinates()
 
-        self.current_offset_x += self.current_speed_x * time_factor
+        speed_x = self.current_speed_x * self.width / 100
+        self.current_offset_x += speed_x * time_factor
 
 class GalaxyApp(App):
     pass
